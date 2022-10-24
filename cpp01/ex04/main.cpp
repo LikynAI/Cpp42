278b257ee93bbf0004ee7cd695c2304db0934ae3
Alex <likyn2001@gmail.com>
2022-10-24 16:09:08 +0300

    done

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -2,27 +2,47 @@
 #include <iostream>
 #include <fstream>
 
-int main()
+std::string ft_replace(std::string text, const std::string find, const std::string replace)
 {
-    std::string filename;
+	size_t	erase_length = find.length();
+	size_t	replace_length = replace.length();
+	size_t	pos_find = 0;
+
+	pos_find = text.find(find);
+	while (pos_find != std::string::npos)
+	{
+		text.erase(pos_find, erase_length);
+		text.insert(pos_find, replace);
+		pos_find = text.find(find, pos_find + replace_length);
+	}
+	return (text);
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4 || argv[1][0] == '\0' || argv[2][0] == '\0')
+        return 1;
+    
+    std::string filename = argv[1];
+    char *find = argv[2];
+    char *replace = argv[3];
     std::string str;
-    std::string find;
-    std::string replace;
-
-    std::cin >> filename >> find >> replace;
-
     std::fstream read;
     std::fstream write;
-    read.open(filename, std::fstream::in | std::fstream::out | std::fstream::app);
-    write.open(filename+".replace", std::fstream::in | std::fstream::out | std::fstream::app);
     
-    while (read >> str)
+    read.open(argv[1], std::fstream::in | std::fstream::out | std::fstream::app);    
+    if (!read.is_open())
+        return 1;
+    
+    write.open((filename+".replace").c_str(), std::fstream::in | std::fstream::out | std::fstream::trunc);
+    if (!read.is_open())
     {
-        std::cout << str;
-        if (str == find)
-            write << replace;
-        else write << str;
+        read.close();
+        return 1;
     }
+    while (std::getline(read, str))
+		write << ft_replace(str, find, replace) << '\n';
     write.close();
     read.close();
 }
