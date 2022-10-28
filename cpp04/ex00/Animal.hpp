28706d2d35d1d3e63afbecc4be1de73790b54141
Alex <likyn2001@gmail.com>
2022-10-28 12:32:21 +0300

    04-00

--- cpp04/ex00/Animal.hpp
+++ cpp04/ex00/Animal.hpp
@@ -1,21 +1,16 @@
+#pragma once
 #include <iostream>
+#include <string>
 
 class Animal
 {
-private:
-    /* data */
+protected:
+	std::string type;
 public:
     Animal(/* args */);
     Animal(const Animal &src);
     Animal &operator=(const Animal &src);
-    ~Animal();
+    virtual void makeSound(void)const;
+    virtual ~Animal();
+    std::string getType(void)const;
 };
-
-Animal::Animal(/* args */)
-{
-
-}
-
-Animal::~Animal()
-{
-}
